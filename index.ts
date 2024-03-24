a0dfaad82e62fe7f83333bdb1e9c6b77603f12e6
Stephen McElhinney <stephen@appstruct.io>
2024-03-24 21:19:48 +0000

    Adds error handling

--- index.ts
+++ index.ts
@@ -26,21 +26,25 @@ if (!existsSync(outputFolder)) {
 const playlist = parser.parse(readFileSync(file, "utf-8"));
 // console.log(JSON.stringify(playlist.items, null, 2));
 for (const item of playlist.items) {
-  const { name, url } = item;
-  // Create a folder in the output folder with the name of the playlist
-  const folder = `${outputFolder}/${name}`;
-//   console.log({ folder });
+  try {
+    const { name, url } = item;
+    // Create a folder in the output folder with the name of the playlist
+    const folder = `${outputFolder}/${name}`;
+    //   console.log({ folder });
 
-  // Create the folder if it doesn't exist
-  if (!existsSync(folder)) {
-    mkdirSync(folder);
+    // Create the folder if it doesn't exist
+    if (!existsSync(folder)) {
+      mkdirSync(folder);
+    }
+
+    // Create a file in the newly created folder with the name ${name}.strm
+    const streamFile = `${folder}/${name}.strm`;
+
+    // Create the file with the path of streamFile and add the url property to that file
+    writeFileSync(streamFile, url);
+
+    console.log(`Created ${streamFile}`);
+  } catch (error) {
+    console.log(error);
   }
-
-  // Create a file in the newly created folder with the name ${name}.strm
-  const streamFile = `${folder}/${name}.strm`;
-
-  // Create the file with the path of streamFile and add the url property to that file
-  writeFileSync(streamFile, url);
-
-  console.log(`Created ${streamFile}`);
 }
