4aa6f0932707c63cad4c4053d9af46f1022a31c7
Stephen McElhinney <stephen@appstruct.io>
2024-03-25 19:49:12 +0000

    Separated into tv movie playlist processing

--- index.ts
+++ index.ts
@@ -2,15 +2,16 @@ import parser from "iptv-playlist-parser";
 import { readFileSync, writeFileSync } from "fs";
 import { mkdirSync, existsSync } from "fs";
 import { resolve } from "path";
+import { processAsMoviePlaylist, processAsTvSeriesPlaylist } from "./processors";
 
-const [file, output] = process.argv.slice(2);
+const [file, output, type] = process.argv.slice(2);
 
 const usage = () => {
-  console.log("Usage: node index.ts <file> <output>");
-  console.log("Example: node index.ts file.m3u8 output.txt");
+  console.log("Usage: node index.ts <file> <output> <type: tv|movie>");
+  console.log("Example: node index.ts file.m3u8 output.txt tv");
 };
 
-if (!file || !output) {
+if (!file || !output || !type) {
   usage();
   process.exit(1);
 }
@@ -24,27 +25,11 @@ if (!existsSync(outputFolder)) {
 }
 
 const playlist = parser.parse(readFileSync(file, "utf-8"));
-// console.log(JSON.stringify(playlist.items, null, 2));
-for (const item of playlist.items) {
-  try {
-    const { name, url } = item;
-    // Create a folder in the output folder with the name of the playlist
-    const folder = `${outputFolder}/${name}`;
-    //   console.log({ folder });
 
-    // Create the folder if it doesn't exist
-    if (!existsSync(folder)) {
-      mkdirSync(folder);
-    }
+if (type === "tv") {
+  processAsTvSeriesPlaylist(playlist, outputFolder);
+}
 
-    // Create a file in the newly created folder with the name ${name}.strm
-    const streamFile = `${folder}/${name}.strm`;
-
-    // Create the file with the path of streamFile and add the url property to that file
-    writeFileSync(streamFile, url);
-
-    console.log(`Created ${streamFile}`);
-  } catch (error) {
-    console.log(error);
-  }
+if(type === "movie") {
+  processAsMoviePlaylist(playlist, outputFolder);
 }
---
+++ processors.ts
@@ -0,0 +1,67 @@
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+
+export const processAsMoviePlaylist = (parsedPlaylist: any, outputFolder: string) => {
+    // console.log(JSON.stringify(playlist.items, null, 2));
+    for (const item of parsedPlaylist.items) {
+      try {
+        const { name, url } = item;
+
+        // Create a folder in the output folder with the name of the playlist
+        const folder = `${outputFolder}/${name}`;
+  
+        // Create the folder if it doesn't exist
+        if (!existsSync(folder)) {
+          mkdirSync(folder);
+        }
+  
+        // Create a file in the newly created folder with the name ${name}.strm
+        const streamFile = `${folder}/${name}.strm`;
+  
+        // Create the file with the path of streamFile and add the url property to that file
+        writeFileSync(streamFile, url);
+  
+        console.log(`Created ${streamFile}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  
+
+export const processAsTvSeriesPlaylist = (parsedPlaylist: any, outputFolder: string) => {
+    // console.log(JSON.stringify(playlist.items, null, 2));
+    for (const item of parsedPlaylist.items) {
+      try {
+        const { name, url } = item;
+        let directory;
+        if (name) {
+          directory = name.split(" ");
+          directory.pop();
+          directory = directory.join(" ");
+        }
+  
+        // console.log({ directory, name, url });
+        // Create a folder in the output folder with the name of the playlist
+        const folder = `${outputFolder}/${directory}`;
+        console.log({ folder });
+  
+        // //   console.log({ folder });
+  
+        // Create the folder if it doesn't exist
+        if (!existsSync(folder)) {
+          mkdirSync(folder);
+        }
+  
+        // Create a file in the newly created folder with the name ${name}.strm
+        const streamFile = `${folder}/${name}.strm`;
+  
+        // Create the file with the path of streamFile and add the url property to that file
+        writeFileSync(streamFile, url);
+  
+        console.log(`Created ${streamFile}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  
